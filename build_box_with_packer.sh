8ebdb6cbca30ae09540334ef1e723993e85de547
jefeweisen <jefeweisen@users.noreply.github.com>
2015-06-27 11:30:45 -0700

    slightly better upstream automation for .box build with docker 1.6.0

--- build_box_with_packer.sh
+++ build_box_with_packer.sh
@@ -2,11 +2,12 @@
 #
 # see: https://github.com/mitchellh/boot2docker-vagrant-box
 
-#git clone git@github.com:mitchellh/boot2docker-vagrant-box.git
+#git clone git@github.com:jefeweisen/boot2docker-vagrant-box.git -b vary_by_docker_version
 #cd boot2docker-vagrant-box
 vagrant up
 vagrant ssh -c 'cd /vagrant && sudo ./build-iso.sh'
 vagrant destroy --force
 packer build -only=virtualbox-iso template.json
-# now have boot2docker_virtualbox.box
-vagrant box add boot2docker_virtualbox.box --name=boot2docker_virtualbox
+# now have boot2docker_virtualbox_1-6-0.box
+vagrant box add boot2docker_virtualbox_1-6-0.box --name=boot2docker_virtualbox_1-6-0
+
