fd9ecaf1212011865a1923e2f36acf7f61b9c8ee
jefeweisen <jefeweisen@users.noreply.github.com>
2015-06-29 09:11:17 -0700

    Use new makefile in boot2docker-vagrant-box.git, WIP

--- build_box_with_packer.sh
+++ build_box_with_packer.sh
@@ -2,12 +2,10 @@
 #
 # see: https://github.com/mitchellh/boot2docker-vagrant-box
 
-#git clone https://github.com/jefeweisen/boot2docker-vagrant-box.git -b vary_by_docker_version
-#cd boot2docker-vagrant-box
-vagrant up
-vagrant ssh -c 'cd /vagrant && sudo ./build-iso.sh'
-vagrant destroy --force
-packer build -only=virtualbox-iso template.json
+git clone https://github.com/jefeweisen/boot2docker-vagrant-box.git -b vary_by_docker_version
+pushd boot2docker-vagrant-box
+make
+make packer-virtualbox
+popd
 # now have boot2docker_virtualbox_1-6-0.box
 vagrant box add boot2docker_virtualbox_1-6-0.box --name=boot2docker_virtualbox_1-6-0
-
