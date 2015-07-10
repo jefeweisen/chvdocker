a89fbfb5e79ae0ff2a0aea48da2cdaf7220d8a2b
jefeweisen <jefeweisen@users.noreply.github.com>
2015-07-10 10:46:01 -0700

    simplify first time run

--- tools/env.sh
+++ tools/env.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 # usage:
 #
 # . ./env.sh
@@ -9,20 +9,10 @@ errecho () {
     echo "$1" 1>&2
 }
 
-if [ ! -e "$C" ]
-then
-  errecho "could not find docker certificates at: $C"
-  errecho ""
-  errecho "These certificates are present in the generated chvdocker boot"
-  errecho "image.  They can be obtained and placed as:"
-  errecho ""
-  errecho "mkdir -p $C"
-  errecho "scp -i ~/.vagrant.d/insecure_private_key -P 2200 -r docker@127.0.0.1:.docker $C"
-else
-  # set environment:
-  export DOCKER_HOST=tcp://127.0.0.1:2376
-  export DOCKER_CERT_PATH="$C"
-  export DOCKER_TLS_VERIFY=1
-fi
+# set environment:
+export DOCKER_HOST=tcp://127.0.0.1:2376
+export DOCKER_CERT_PATH="$C"
+export DOCKER_TLS_VERIFY=1
+
 
 
--- tools/extract_my_docker_certs.sh
+++ tools/extract_my_docker_certs.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+pushd $(dirname "$0") > /dev/null
+cd ..
+adirRepo=$(pwd)
+popd > /dev/null
+
 set -e
 # TODO: fail gracefully
 
+. "$adirRepo/tools/env.sh"
+echo "DOCKER_CERT_PATH = $DOCKER_CERT_PATH"
 mkdir -p "$DOCKER_CERT_PATH"
 vagrant ssh -c "cat /home/docker/.docker/ca.pem" > "$DOCKER_CERT_PATH/ca.pem"
 vagrant ssh -c "cat /home/docker/.docker/cert.pem" > "$DOCKER_CERT_PATH/cert.pem"
